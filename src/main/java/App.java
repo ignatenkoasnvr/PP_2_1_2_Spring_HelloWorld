e4ffab477f9db5a7f2687df1c38ed3c91a97e8eb
ignatenkoasnvr <ignatenkoasnvr@gmail.com>
2023-06-08 12:28:06 +0300

    Colution(v1)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,25 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+
+        HelloWorld bean3 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean3.getMessage());
+
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(bean.equals(bean3));
+
     }
 }
